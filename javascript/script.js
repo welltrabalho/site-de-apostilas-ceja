0e65f71043c33e7a9c765764d02797e9a78228b2
ANTONIO WELLINGTON DO CARMO VIEIRA <159496788+welltrabalho@users.noreply.github.com>
2024-02-18 20:14:36 -0300

    outro

--- javascript/script.js
+++
@@ -1,7 +0,0 @@
-$('.nordeste').click(function(){
-    $('.menuLateral ul .itensNordeste').toggleClass('mostra');
-});
-
-$('.sudeste').click(function(){
-    $('.menuLateral ul .itensSudeste').toggleClass('mostra');
-});
